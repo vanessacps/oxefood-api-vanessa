a925c01ddecdef32c8988d77f87571b06fe99c10
vanessacps <98425149+vanessacps@users.noreply.github.com>
2023-09-15 14:50:11 -0300

    listagem e alteracão completa

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -9,7 +9,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.produto.Produto;
 
 
 @Service
